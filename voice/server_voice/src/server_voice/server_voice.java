f843d5a9bb76d469f017e8fa4716210529967da0
admin <admin@LAPTOP-K2KH2NRD>
2021-10-29 21:53:22 +0700

    voice

---
+++ voice/server_voice/src/server_voice/server_voice.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server_voice;
+
+/**
+ *
+ * @author admin
+ */
+public class server_voice {
+    public static boolean calling = true;
+    public static void main(String[] args) {
+        server_fr fr = new server_fr();
+        fr.setVisible(true);
+    }
+    
+}
